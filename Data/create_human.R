357eb6984aa1d82b7fd36fca8df890c04a4abdac
Elli Taimela <elli.taimela@fourkind.com>
2018-11-25 19:25:13 +0000

    RStudio e4
    update

--- Data/create_human.R
+++ Data/create_human.R
@@ -25,6 +25,14 @@ str(gii)
 dim(gii)
 summary(gii)
 
+colnames(hd)[3] <- "HDI" # Human.Development.Index..HDI.  
+colnames(hd)[4] <- "Life_exp" # Life.Expectancy.at.Birth 
+colnames(hd)[5] <- "Edu_exp"
+colnames(hd)[6] <- "Edu_mean"
+colnames(hd)[7] <- "GNIpercapita"
+colnames(hd)[8] <- "GNI_HDI_rank"
+
+
 # Mutate gii and add new ratio variables 
 gii <- mutate(gii, edu_ratio = Population.with.Secondary.Education..Female./Population.with.Secondary.Education..Male.)
 gii <- mutate(gii, laborforce_ratio = Labour.Force.Participation.Rate..Female./Labour.Force.Participation.Rate..Male.)
